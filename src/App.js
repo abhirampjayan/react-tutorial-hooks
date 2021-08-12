61b03183f877d1e5df5cd875bfe66be1c5a1a92c
abhirampjayan <abhirampjayan@outlook.com>
2021-08-12 16:38:43 +0530

    hooks basics 1

--- src/App.js
+++ src/App.js
@@ -1,24 +1,51 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import React, { useState, useEffect, useRef } from "react";
 
-function App() {
+function useLocalStorage(
+  key,
+  defaultVal,
+  { serialize = JSON.stringify, deserialize = JSON.parse } = {}
+) {
+  const [status, setStatus] = useState(() => {
+    const x = window.localStorage.getItem(key);
+    if (x) {
+      return deserialize(x);
+    }
+    return typeof defaultVal === "function" ? defaultVal() : defaultVal;
+  });
+
+  const prevKeyRef = useRef(key);
+
+  useEffect(() => {
+    const prevKey = prevKeyRef.current;
+    if (prevKey !== key) {
+      console.log("changed")
+      window.localStorage.removeItem(prevKey);
+    }
+    prevKeyRef.current = key;
+    window.localStorage.setItem(key, serialize(status));
+  }, [key, serialize, status]);
+  return [status, setStatus];
+}
+
+function App({ initialName = "" }) {
+  function handleChange(event) {
+    setName(event.target.value);
+  }
+
+  const [name, setName] = useLocalStorage("name2", initialName);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <h1>Hello {name}</h1>
+      <input
+        type="text"
+        name="name"
+        value={name}
+        onChange={handleChange}
+        id="name"
+      />
+    </>
   );
 }
 
